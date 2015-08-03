4be949e8d577ca74c5296c4e6a832948fcb4dae8
Ronald Tejada <rontejada91@gmail.com>
2015-08-03 16:22:23 -0400

    Added a bubble sort method that accepts blocks

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,6 @@
 require 'Benchmark'
 
+# Unoptimized bubble sort (Optimized version is bubble_sort_check)
 def bubble_sort (the_array)
 
   unsorted = the_array.length - 1
@@ -20,6 +21,7 @@ def bubble_sort (the_array)
   return the_array
 end
 
+# Breaks if no elements were switched during an iteration (completed)
 def bubble_sort_check (the_array)
 
   unsorted = the_array.length - 1
@@ -29,6 +31,33 @@ def bubble_sort_check (the_array)
     switched = false
     while i < unsorted do
       if the_array[i] > the_array[i + 1]
+        temp = the_array[i]
+        the_array[i] = the_array[i + 1]
+        the_array[i + 1] = temp
+        switched = true
+      end
+      i += 1
+    end
+    if switched 
+      return the_array
+    else unsorted -= 1
+    end
+  end
+
+  return the_array
+end
+
+# Takes in a block that compares elements in the array
+# Also breaks if no elements were switched during an iteration
+def bubble_sort_by (the_array)
+
+  unsorted = the_array.length - 1
+
+  while unsorted > 0
+    i = 0
+    switched = false
+    while i < unsorted do
+      if yield(the_array[i], the_array[i + 1]) > 1
         temp = the_array[i]
         the_array[i] = the_array[i + 1]
         the_array[i + 1] = temp
@@ -57,4 +86,4 @@ puts "Original: #{sample_array.inspect}"
 puts "Sorted: #{ bubble_sort(sample_array).inspect }"
 puts "Bubble Sort: #{ Benchmark.realtime { bubble_sort(sample_array) }} seconds"
 puts "With Completion Check: #{ Benchmark.realtime { bubble_sort_check(sample_array) }} seconds"
-
+puts "With Block Provided: #{ Benchmark.realtime { bubble_sort_by(sample_array) { |num1, num2| num1 - num2 }}} seconds"
